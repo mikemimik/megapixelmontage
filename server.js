df23d9676fc2554b8bcb11150e42558c667cce48
Michael Perrotte <mike@mikecorp.ca>
2025-05-20 01:44:11 -0400

    feat(server): updated server config
    - add @fastify/env configuration
    - add S3 client decorator
    - use environment variables

--- server.js
+++ server.js
@@ -1,10 +1,48 @@
 import Fastify from "fastify";
 import FastifyVite from "@fastify/vite";
+import FastifyEnv from "@fastify/env";
+// TODO: add `@fastify/caching` to enable to `cache` object decorated on the
+// server (to get leveraged by server side rendering)
+
+import { S3 } from "@aws-sdk/client-s3";
 
 const server = Fastify({
   logger: {
     transport: {
       target: "@fastify/one-line-logger",
+    },
+  },
+});
+
+await server.register(FastifyEnv, {
+  dotenv: true,
+  schema: {
+    type: "object",
+    required: [
+      "PORT",
+      "DO_SPACE_ACCESS_KEY_ID",
+      "DO_SPACE_SECRET_KEY",
+      "DO_SPACE_BUCKET",
+    ],
+    properties: {
+      PORT: {
+        type: "string",
+      },
+      DO_SPACE_ACCESS_KEY_ID: {
+        type: "string",
+        nullable: false,
+        minLength: 1,
+      },
+      DO_SPACE_SECRET_KEY: {
+        type: "string",
+        nullable: false,
+        minLength: 1,
+      },
+      DO_SPACE_BUCKET: {
+        type: "string",
+        nullable: false,
+        minLength: 1,
+      },
     },
   },
 });
@@ -16,4 +54,18 @@ await server.register(FastifyVite, {
 });
 
 await server.vite.ready();
-await server.listen({ port: 3002 });
+
+server.decorate(
+  "s3Client",
+  new S3({
+    forcePathStyle: false,
+    endpoint: "https://tor1.digitaloceanspaces.com",
+    region: "tor1",
+    credentials: {
+      accessKeyId: server.getEnvs().DO_SPACE_ACCESS_KEY_ID,
+      secretAccessKey: server.getEnvs().DO_SPACE_SECRET_KEY,
+    },
+  }),
+);
+
+await server.listen({ port: server.getEnvs().PORT });
