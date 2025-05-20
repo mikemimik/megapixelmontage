257beb1ac9ebeeafd82b1cf59a769d8ca82233a3
Michael Perrotte <mike@mikecorp.ca>
2025-05-20 16:40:53 -0400

    feat(client/context): add context caching
    - cache the values we get when listing the bucket

--- client/context.js
+++ client/context.js
@@ -1,68 +1,92 @@
 import { ListObjectsV2Command } from "@aws-sdk/client-s3";
 
+const IMAGE_LIST_KEY = "image:list";
+
+function createGroups(objects = []) {
+  const ALL = "all";
+  const GENERAL = "general";
+
+  return objects.reduce((acc, object) => {
+    const parts = object.Key.split("/");
+
+    // TODO: figure out if there is a better way to do this double assign
+    const collection = parts[0];
+    const first = parts[0];
+    const last = parts[parts.length - 1];
+
+    const item = {
+      name: object.Key,
+      tag: object.ETag,
+    };
+
+    // INFO: if filename starts with underscore, skip it
+    if (first.startsWith("_")) {
+      return acc;
+    }
+
+    if (!acc[ALL]) {
+      acc[ALL] = [];
+    }
+
+    if (parts.length === 1) {
+      if (!acc[GENERAL]) {
+        acc[GENERAL] = [];
+      }
+
+      acc[GENERAL].push(item);
+      acc[ALL].push(item);
+      return acc;
+    }
+
+    if (Object.keys(acc).includes(collection)) {
+      acc[collection].push(item);
+      acc[ALL].push(item);
+      return acc;
+    }
+
+    if (!last) {
+      // INFO: last part is falsy
+      // create the collection
+      if (!acc[collection]) {
+        acc[collection] = [];
+      }
+    }
+
+    return acc;
+  }, {});
+}
+
 export default async (ctx) => {
+  console.group("context.js");
+  console.log("server:", !!ctx.server);
   if (ctx.server) {
-    const bucket = ctx.server.getEnvs().DO_SPACE_BUCKET;
-    const command = new ListObjectsV2Command({
-      Bucket: bucket,
-    });
-    const response = await ctx.server.s3Client.send(command);
+    const cachedGroups = ctx.server.cache.get(IMAGE_LIST_KEY);
 
-    if (response.Contents) {
-      const groups = response.Contents.reduce((acc, object) => {
-        const parts = object.Key.split("/");
-        // TODO: figure out if there is a better way to do this double assign
-        const collection = parts[0];
-        const first = parts[0];
-        const last = parts[parts.length - 1];
-        const ALL = "all";
-        const GENERAL = "general";
-        const item = {
-          name: object.Key,
-          tag: object.ETag,
-        };
+    if (!cachedGroups) {
+      console.log("cache miss:", IMAGE_LIST_KEY, cachedGroups);
+      const bucket = ctx.server.getEnvs().DO_SPACE_BUCKET;
 
-        // INFO: if filename starts with underscore, skip it
-        if (first.startsWith("_")) {
-          return acc;
-        }
+      const command = new ListObjectsV2Command({
+        Bucket: bucket,
+      });
 
-        if (!acc[ALL]) {
-          acc[ALL] = [];
-        }
+      const response = await ctx.server.s3Client.send(command);
 
-        if (parts.length === 1) {
-          if (!acc[GENERAL]) {
-            acc[GENERAL] = [];
-          }
-
-          acc[GENERAL].push(item);
-          acc[ALL].push(item);
-          return acc;
-        }
-
-        if (Object.keys(acc).includes(collection)) {
-          acc[collection].push(item);
-          acc[ALL].push(item);
-          return acc;
-        }
-
-        if (!last) {
-          // INFO: last part is falsy
-          // create the collection
-          if (!acc[collection]) {
-            acc[collection] = [];
-          }
-        }
-
-        return acc;
-      }, {});
-
-      const { all, ...rest } = groups;
+      if (response.Contents) {
+        const groups = createGroups(response.Contents);
+        ctx.server.cache.set(IMAGE_LIST_KEY, groups);
+        const { all, ...rest } = groups;
+        ctx.state.all = all;
+        ctx.state.groups = { ...rest };
+      }
+    } else {
+      console.log("cache hit:", IMAGE_LIST_KEY, cachedGroups);
+      const { all, ...rest } = cachedGroups;
       ctx.state.all = all;
       ctx.state.groups = { ...rest };
     }
   }
+  console.groupEnd();
 };
 
 export function state() {
