28580eb6b490bbcb42dacbc4f576ee1a9d209fb7
Michael Perrotte <mike@mikecorp.ca>
2025-05-20 17:50:42 -0400

    feat(client/components): add MainSection component

---
+++ client/components/MainSection.jsx
@@ -0,0 +1,57 @@
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid2";
+
+import Column from "./Column";
+
+import { useRouteContext } from "@fastify/react/client";
+
+const MainSection = () => {
+  const { state, data } = useRouteContext();
+
+  console.group("MainSection");
+  console.log("data:", data);
+  console.groupEnd();
+
+  const [leftGrid, middleGrid, rightGrid] = state.all.reduce(
+    (acc, item, index) => {
+      const left = acc[0] ?? [];
+      const middle = acc[1] ?? [];
+      const right = acc[2] ?? [];
+
+      const imageData = {
+        image: `https://cdn.megapixelmontage.ca/${item.name}`,
+        description: data[item.name].description,
+        title: data[item.name].title,
+      };
+      const sort = index % 3;
+      if (sort === 0) {
+        left.push(imageData);
+      } else if (sort === 1) {
+        middle.push(imageData);
+      } else if (sort === 2) {
+        right.push(imageData);
+      }
+
+      return [left, middle, right];
+    },
+    [],
+  );
+
+  return (
+    <Box>
+      <Grid container spacing={4}>
+        <Grid size={{ xs: 12, md: 4 }}>
+          <Column data={leftGrid} />
+        </Grid>
+        <Grid size={{ xs: 12, md: 4 }}>
+          <Column data={middleGrid} />
+        </Grid>
+        <Grid size={{ xs: 12, md: 4 }}>
+          <Column data={rightGrid} />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default MainSection;
