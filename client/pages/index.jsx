d5b90591bbf5492db56f3313cd4fcc7c1e216f5a
Michael Perrotte <mike@mikecorp.ca>
2025-05-20 17:59:13 -0400

    feat(client/pages): utilise Hero and Container components

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -14,6 +14,8 @@ import { useRouteContext } from "@fastify/react/client";
 import Footer from "../components/Footer";
 import Contact from "../components/Contact";
 import MainSection from "../components/MainSection";
+import Hero from "../components/Hero";
+import Container from "../components/Container";
 
 export const serverOnly = true;
 
@@ -68,20 +70,6 @@ export function getMeta() {
   };
 }
 
-// INFO: components/container.js
-const Container = ({ children, ...rest }) => (
-  <Box
-    maxWidth={{ sm: 720, md: 1236 }}
-    width={1}
-    margin={"0 auto"}
-    paddingX={2}
-    paddingY={{ xs: 4, sm: 6, md: 8 }}
-    {...rest}
-  >
-    {children}
-  </Box>
-);
-
 // INFO: components/partners.js
 
 const Partners = () => {
@@ -117,93 +105,6 @@ const Partners = () => {
           />
         </Box>
       ))}
-    </Box>
-  );
-};
-// INFO: components/hero.js
-const Hero = ({ url }) => {
-  const theme = useTheme();
-
-  const defaultHeroImageSource =
-    "https://assets.maccarianagency.com/backgrounds/img1.jpg";
-  const heroImageSource = url ?? defaultHeroImageSource;
-
-  return (
-    <Box
-      position={"relative"}
-      sx={{
-        backgroundImage: `url("${heroImageSource}")`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        marginTop: -13,
-        paddingTop: 13,
-        "&:after": {
-          position: "absolute",
-          content: '" "',
-          width: "100%",
-          height: "100%",
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-          zIndex: 1,
-          background: "#161c2d",
-          opacity: 0.6,
-        },
-      }}
-    >
-      <Container
-        zIndex={3}
-        position={"relative"}
-        minHeight={{ xs: 300, sm: 400, md: 600 }}
-        display={"flex"}
-        alignItems={"center"}
-        justifyContent={"center"}
-      >
-        <Box>
-          <Box marginBottom={2}>
-            <Typography
-              variant="h2"
-              align={"center"}
-              sx={{
-                fontWeight: 700,
-                color: theme.palette.common.white,
-              }}
-            >
-              Megapixel Montage
-            </Typography>
-          </Box>
-          <Box>
-            <Typography
-              variant="h4"
-              align={"center"}
-              sx={{
-                fontWeight: 700,
-                color: theme.palette.common.white,
-              }}
-            >
-              Street Style Photography
-            </Typography>
-          </Box>
-        </Box>
-      </Container>
-      <Box
-        component={"svg"}
-        preserveAspectRatio="none"
-        xmlns="http://www.w3.org/2000/svg"
-        x="0px"
-        y="0px"
-        viewBox="0 0 1920 100.1"
-        width={1}
-        marginBottom={-1}
-        position={"relative"}
-        zIndex={2}
-      >
-        <path
-          fill={theme.palette.background.paper}
-          d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"
-        ></path>
-      </Box>
     </Box>
   );
 };
