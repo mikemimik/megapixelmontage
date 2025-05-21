39dd9935743c47df0ecb8adf69edbf515620af03
Michael Perrotte <mike@mikecorp.ca>
2025-05-20 23:49:24 -0400

    chore: update vite config
    - remove warnings

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,25 @@ import { resolve } from "node:path";
 import viteReact from "@vitejs/plugin-react";
 import viteFastifyReact from "@fastify/react/plugin";
 
+const WARNING_CODES = ["MODULE_LEVEL_DIRECTIVE"];
+
 export default {
   root: resolve(import.meta.dirname, "client"),
   plugins: [viteReact(), viteFastifyReact()],
   ssr: {
     noExternal: ["@mui/*"],
+  },
+  build: {
+    rollupOptions: {
+      onLog(level, log, handler) {
+        if (level === "warn" && log.code) {
+          if (WARNING_CODES.includes(log.code)) {
+            return;
+          }
+        } else {
+          handler(level, log);
+        }
+      },
+    },
   },
 };
