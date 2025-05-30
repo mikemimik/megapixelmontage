f8ed2ffdc7332400a3a5580b537c3cd637293981
Michael Perrotte <mike@mikecorp.ca>
2025-05-30 00:20:58 -0400

    feat(client/components): add AsyncImage component

---
+++ client/components/AsyncImage.jsx
@@ -0,0 +1,20 @@
+import { InView } from "react-intersection-observer";
+
+import ImageContainer from "./ImageContainer";
+import Image from "./Image";
+
+export const clientOnly = true;
+
+export default function AsyncImage({ photo, initialInView = false }) {
+  return (
+    <InView rootMargin="600px 0px" initialInView={initialInView} triggerOnce>
+      {({ ref, inView }) => {
+        return (
+          <ImageContainer ref={ref}>
+            <Image photo={photo} inView={inView} />
+          </ImageContainer>
+        );
+      }}
+    </InView>
+  );
+}
