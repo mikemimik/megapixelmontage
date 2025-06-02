bf40733d33afdfd1024f4cc3a50f86fab59bee91
Michael Perrotte <mike@mikecorp.ca>
2025-06-01 23:48:56 -0400

    feat(utils): update cache plugin
    - extend to event emitter
    - emit when cache has been emptied, due to evictions

--- utils/plugin-cache.js
+++ utils/plugin-cache.js
@@ -1,10 +1,15 @@
+import { EventEmitter } from "node:events";
 import fp from "fastify-plugin";
 import TTLCache from "@isaacs/ttlcache";
 
 // INFO: Cache Plugin
-class Cache {
+class Cache extends EventEmitter {
   constructor(options) {
+    super();
     const { log, ...cacheOptions } = options;
+
+    // INFO: required because of context switching below
+    const self = this;
 
     // INFO: set logger to use
     this.log = log;
@@ -12,19 +17,29 @@ class Cache {
     // INFO: create cache instance
     this.cache = new TTLCache({
       ttl: 60 * 60 * 1_000 * 1, // one hour
-      updateAgeOnGet: true,
-      dispose(value, key, reason) {
-        this.log.info({ value, key, reason }, `cache:displose - ${key}`);
+      dispose(_value, key, reason) {
+        self.log.info(
+          { key, reason, cacheSize: self.cache.size },
+          `cache:displose - ${key} - ${reason}`,
+        );
+
+        if (self.cache.size === 0) {
+          self.emit("cache:invalidation", {
+            cacheSize: self.cache.size,
+          });
+        }
       },
       ...cacheOptions,
     });
   }
 
   set(key, value) {
+    this.log.debug({ key }, "setting key");
     return this.cache.set(key, value);
   }
 
   get(key) {
+    this.log.debug({ key }, "getting key");
     return this.cache.get(key);
   }
 }
